687a4eade16d516326b49728010475193b58db9a
Reinaldo Clemente <rclemente@brasilcash.onmicrosoft.com>
2020-10-19 03:42:56 -0300

    Reinaldo add a new modal

--- src/pages/NossaCarta/index.tsx
+++ src/pages/NossaCarta/index.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
-import { Container } from "./styles";
+import { Container, ModalTrash2 } from "./styles";
+
 import { TiArrowBack } from "react-icons/ti";
 import { FiTrash } from "react-icons/fi";
 import { HiOutlinePencil } from "react-icons/hi";
+
 import data from "../../drinks.json";
 
 const drinkList = data.drinks;
 
-const useModal = () => {
-  const [isShowing, setIsShowing] = useState(false);
-
-
 const NossaCartaPages: React.FC = () => {
+
+  const [modalDrink, setModalDrink] = useState(false);
 
   return (
     <Container>
@@ -29,15 +29,24 @@ const NossaCartaPages: React.FC = () => {
                 <img src={drink.url_img} />
               </div>
               <h4>{drink.drink_name}</h4>
-              <button onClick={toggle}><FiTrash className="__delete"/></button>
+              <button onClick={() => setModalDrink(true)}><FiTrash className="__delete"/></button>
               <button><HiOutlinePencil className="__update"/></button>
             </div>
           ))}
       </div>
-      <Modal
-        isOpen = {isShowing}
-        isClose = {toggle}
-      />
+      {modalDrink &&
+       <ModalTrash2 className="teste">
+        <div>
+          <button className="__close" onClick={() => setModalDrink(false)}>&times;</button>
+          <p>Tem certeza que deseja apagar um elemento?</p>
+          <ul>
+            <li><button>Sim</button></li>
+            <li><button>Não</button></li>
+          </ul>
+        </div>
+       </ModalTrash2>
+      }
+
     </Container>
   );
 };
--- src/pages/NossaCarta/styles.ts
+++ src/pages/NossaCarta/styles.ts
@@ -156,4 +156,92 @@ export const Container = styled.div`
       right: -120%;
     }
   }
-  `
+`
+export const ModalTrash2 = styled.div`
+position: fixed;
+width: 100vw;
+height: 100vh;
+background: rgba(125,125,125,0.5);
+transform: translateY(0px);
+animation: moveToBottom .4s;
+
+@keyframes moveToBottom {
+  from {
+    transform: translateY(-20px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+}
+
+div {
+  background: linear-gradient(-45deg, #FDCB9E, #FDCBfE);
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  width: 600px;
+  height: 150px;
+  box-shadow: 0px 10px 25px rgba(0,0,0,0.45);
+  border-radius: 5px;
+  overflow: hidden;
+
+  .__close {
+    background: #cacaca;
+    position: absolute;
+    right: 0%;
+    width: 20px;
+    height: 20px;
+    text-align: center;
+    margin: 10px;
+    padding: 2.5px;
+    box-shadow: 0px 10px 15px rgba(0,0,0,0.25);
+    transition: .4s;
+    cursor: pointer;
+
+    :hover {
+      background: white;
+      box-shadow: 0px 5px 5px rgba(0,0,0,0.5);
+    }
+  }
+
+  p {
+    flex: 1;
+    color: black;
+    width: 100%;
+    padding: 25px;
+    font-weight: bolder;
+  }
+
+  ul {
+    flex: 1;
+    list-style: none;
+    display: flex;
+    color: white;
+    width: 100%;
+  }
+
+  li {
+    display: inline-block;
+    padding: 25px;
+  }
+
+  button {
+    padding: 10px 25px ;
+    border: 0;
+    border-radius: 5px;
+    background: #cacaca;
+    box-shadow: 0px 10px 15px rgba(0,0,0,0.25);
+    transition: .4s;
+    cursor: pointer;
+
+    :hover {
+      background: white;
+      box-shadow: 0px 5px 5px rgba(0,0,0,0.5);
+    }
+  }
+}`
